2eb57a37057f880d41ce73f74ac7a25531ec75fa
jsanford <jsanford@unb.ca>
2018-07-16 08:38:27 -0300

    Update max shelves

--- custom/modules/cabinetry/cabinetry_cabinet_project/src/Entity/CabinetModule.php
+++ custom/modules/cabinetry/cabinetry_cabinet_project/src/Entity/CabinetModule.php
@@ -113,7 +113,7 @@ class CabinetModule extends PhysicalObject implements CabinetModuleInterface {
         [
           'default_value' => 0,
           'min' => 0,
-          'max' => 10,
+          'max' => 5,
           'step' => 1,
         ]
       )
